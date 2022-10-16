60105e99fcba064b87827f033b3d5f5a18402a41
Varun Bogulla <varunub1505@gmail.com>
2022-10-17 01:12:19 +0530

    updated ports

--- app.js
+++ app.js
@@ -195,7 +195,10 @@ app.get("/:para_name",(req,res)=>{
 })
 
 
-
-app.listen(3000,function(){
+let port = process.env.PORT
+if(port == null || port==""){
+    port=3000
+}
+app.listen(port,function(){
     console.log("Server started at port 3000")
 })
